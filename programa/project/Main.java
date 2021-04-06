eab4308257857995a7acdeac70a0f6e951c58d94
Levi <arcanjolevi@gmail.com>
2021-04-06 14:35:44 -0400

    program update

--- programa/project/Main.java
+++ programa/project/Main.java
@@ -16,6 +16,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT nomePro FROM produto");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome e codigo de todos os produtos cujo o precoVenda seja maior que 100:");
@@ -24,6 +26,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT nomePro, codPro FROM produto WHERE precoVenda > 100");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o codigo de todos os produtos que tenham registro de compra e venda:");
@@ -32,6 +36,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT codPro FROM ItemCompra INTERSECT SELECT codPro FROM ItemVenda");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o codigo de todos os produtos que nao tem registro de venda ainda:");
@@ -40,6 +46,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT codPro FROM produto Except SELECT codPro FROM ItemVenda");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o codigo de todos os produtos que nao tem nenhum registro de compra e venda:");
@@ -48,6 +56,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT codPro FROM produto Except (SELECT codPro FROM ItemCompra UNION SELECT codPro FROM ItemVenda)");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome de todos os clientes cadastrados:");
@@ -56,6 +66,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT nome FROM Cliente");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome de todos os produtos que tem venda:");
@@ -64,6 +76,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT DISTINCT nomePro FROM produto, ItemVenda WHERE produto.codPro = ItemVenda.codPro");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome dos clientes que tem notaVenda, cuja a venda foi realiazada em 2021:");
@@ -72,6 +86,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT nome FROM NotaVenda, Cliente WHERE NotaVenda.codCliente = Cliente.codCliente AND YEAR(NotaVenda.dataVenda) = 2021");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome de todos os produtos que nao tem Venda registrada:");
@@ -80,6 +96,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT nomePro FROM produto Except (SELECT produto.nomePro FROM produto, ItemVenda WHERE produto.codPro = ItemVenda.codPro)");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome de todos os produtos que tem Compra, mas nao tem venda registrada:");
@@ -88,6 +106,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT nomePro FROM produto, ItemCompra WHERE produto.codPro = ItemCompra.codPro Except SELECT nomePro FROM produto, ItemVenda WHERE produto.codPro = ItemVenda.codPro;");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome de todos os clientes que ainda nao tem venda registrada em 2021:");
@@ -96,6 +116,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT codCliente FROM Cliente Except SELECT codCliente FROM NotaVenda WHERE YEAR(NotaVenda.dataVenda) = 2021");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o produto que tem o maior preco de venda cadastrado:");
@@ -104,6 +126,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("select produto.precoVenda, produto.nomePro from produto Except select produto.precoVenda, produto.nomePro from produto join produto v2 on produto.precoVenda < v2.precoVenda");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Considere que o cliente possa ter profissao informada ou nao. Sendo assim liste o nome de todos os Clientes cadastrados com o nome da sua profissao, caso tenham:");
@@ -112,6 +136,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT Cliente.nome, Profissao.nomeProfissao FROM Cliente LEFT JOIN Profissao ON Profissao.idProfissao = Cliente.idProfissao");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Considere que o cliente possa ter profissao informada ou nao. Sendo assim listeo nome dos Clientes cadastrados com o nome de sua profissao, apenas para os clientes com profissao informada:");
@@ -120,6 +146,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT Cliente.nome,Profissao.nomeProfissao FROM Cliente RIGHT JOIN Profissao ON Cliente.idProfissao = Profissao.idProfissao where Cliente.nome is not null");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 
 		
 		System.out.println("---> Liste o nome dos produtos que tenham registros tanto em ItemVenda, quanto em itemCompra em Marco 2021:");
@@ -128,6 +156,8 @@ public class Main {
 		reader.nextLine();
 		conn.queryAndPrint("SELECT produto.nomePro FROM NotaVenda INNER JOIN ItemVenda on ItemVenda.nroVenda = NotaVenda.nroVenda INNER JOIN produto on produto.codPro = ItemVenda.codPro WHERE YEAR(NotaVenda.dataVenda) = '2021' AND MONTH(NotaVenda.dataVenda) = '03' UNION SELECT produto.nomePro FROM NotaCompra INNER JOIN ItemCompra on ItemCompra.nroCompra = NotaCompra.nroCompra INNER JOIN produto on produto.codPro = ItemCompra.codPro WHERE YEAR(NotaCompra.dataCompra) = '2021' AND MONTH(NotaCompra.dataCompra) = '03'");
 		System.out.println("");
+		reader.nextLine();
+		System.out.print("\033[H\033[2J");  
 		
 		
 		conn.closeConnection();
